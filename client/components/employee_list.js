c2f4fb6ef01715a8d40f30f053bf1ca7b30bcda0
JamesWinestock <james.winestock@gmail.com>
2016-07-12 13:31:34 +1000

    users appearing with flexbox arrangement

---
+++ client/components/employee_list.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Employees } from '../../imports/collections/employees';
+import EmployeeDetail from './employee_detail';
+
+const PER_PAGE = 20;
+
+class EmployeeList extends Component {
+render() {
+
+    return (
+      <div>
+        <div className="employee-list">
+          {this.props.employees.map(employee => <EmployeeDetail key={employee._id} employee={employee} />
+          )}
+        </div>
+        <button onClick={ () => Meteor.subscribe('employees', 40) }
+        className="btn btn-primary">Load more...</button>
+      </div>
+    );
+  }
+};
+
+export default createContainer(() => {
+  // set up subscription
+  Meteor.subscribe('employees', PER_PAGE);
+  // return an object
+  return { employees: Employees.find({}).fetch() };
+}, EmployeeList);
